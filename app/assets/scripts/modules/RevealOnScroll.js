75e9dcada7e454d88c8396892bc31711ead241e7
Anniek <anniek.bisschop.dev@gmail.com>
2020-12-16 12:29:39 +0100

    added funcionality to services and testimonials

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,28 +1,54 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+
 class RevealOnScroll {
-    constructor(){
-        this.itemsToReveal = document.querySelectorAll(".feature-item")
+    constructor(elements, thresholdPercent){
+        this.thresholdPercent = thresholdPercent
+        this.itemsToReveal = elements
+        this.browserHeight = window.innerHeight
         this.hideInitially()
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
         this.events()
     }
 
     events(){
-        window.addEventListener("scroll", () => {
-            this.itemsToReveal.forEach(element => {
-                this.calculateIsScrolledto(element)
-            })
+        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener("resize", debounce(() => {
+            console.log("resize")
+            this.browserHeight = window.innerHeight
+        }, 300))
+    }
+
+    calcCaller(){
+        console.log("scrol function ran");
+        this.itemsToReveal.forEach(element => {
+            if(element.isRevealed == false){
+                this.calculateIfScrolledto(element)
+            }
         })
     }
 
-    calculateIsScrolledto(element){
-        let scrollPercent = (element.getBoundingClientRect().y / window.innerHeight) * 100
-        if(scrollPercent < 75 ){
+    calculateIfScrolledto(element){
+       if(window.scrollY + this.browserHeight > element.offsetTop){
+        console.log("element calculated")
+        let scrollPercent = (element.getBoundingClientRect().y / this.browserHeight) * 100
+        if(scrollPercent < this.thresholdPercent ){
             element.classList.add("reveal-item--is-visible")
+            element.isRevealed = true
+            if (element.isLastItem){
+                window.removeEventListener("scroll", this.scrollThrottle)
+            }
         }
-
+       }
     }
 
     hideInitially(){
-        this.itemsToReveal.forEach(element => element.classList.add("reveal-item"))
+        this.itemsToReveal.forEach(element => {
+            element.classList.add("reveal-item")
+            element.isRevealed = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
     }
 
 
