930bcdc30565bb8e9d682d7667bcec7aa5d39151
Anniek <anniek.bisschop.dev@gmail.com>
2020-12-17 22:05:02 +0100

    changed folder output name

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const postCSSPlugins = [
 class RunAfterCompile{
     apply(compiler) {
         compiler.hooks.done.tap('Copy images', function() {
-            fse.copySync('./app/assets/img', './dist/assets/img')
+            fse.copySync('./app/assets/img', './docs/assets/img')
         })
     }
 }
@@ -70,7 +70,7 @@ if (currentTask == 'build'){
     config.output = {
         filename: '[name].[chunkhash].js',
         chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     }
     config.mode = 'production'
     config.optimization = {
